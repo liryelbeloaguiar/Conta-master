8fd06705b1c9f8dbc77a5e17f2d83ba96471b674
liryelbeloaguiar <133534040+liryelbeloaguiar@users.noreply.github.com>
2023-05-13 16:31:43 -0300

    Add files via upload

---
+++ Conta-master/Conta-master/Conta/src/sample/ContaBancaria.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ContaBancaria {
+
+    @FXML
+    private Button ButtonCC;
+
+    @FXML
+    private Button ButtonCP;
+    
+}
